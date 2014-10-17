62f132255aef99500975f38477fab8a69e4f2d4e
Hai Kieu <haikieu2907@gmail.com>
2014-10-17 12:26:47 +0700

    Create anti pattern puppet project

---
+++ PuppetAntiPattern/PuppetAntiPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PuppetAntiPattern
+//
+//  Created by HK on 10/17/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PuppetAntiPattern/PuppetAntiPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PuppetAntiPattern
+//
+//  Created by HK on 10/17/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
